6695f1d5026deae3d609ca939dc27e3e30eaa540
Shreyas <sovanishreyas@gmail.com>
2025-09-18 14:08:57 +0000

    Automated address retrival of deployed ignition

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,30 @@
 const hre = require("hardhat");
-
+const fs = require("fs");
+const path = require("path");
 
 async function main() {
 
-    const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Replace with your deployed contract address
+    //  get the current network name
+    const network = hre.network.name;
+    const chainId = hre.network.config.chainId;
+    const deploymentDir = `chain-${chainId}`;
+    console.log("Network:", network);
+
+    //path to ignition deployments artifacts
+    const deploymentPath = path.join(
+        __dirname, 
+        "..", 
+        "ignition", 
+        "deployments", 
+        deploymentDir, 
+        "deployed_addresses.json");
+        
+    //read the deployment file
+    const deployment = JSON.parse(fs.readFileSync(deploymentPath, "utf8"));
+    console.log("Deployment:", deployment);
+    
+    // get the deployed contract address
+    const address = deployment["SimpleStorageModule#SimpleStorage"];
 
     // Attach to the deployed contract
     const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
